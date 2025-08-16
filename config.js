2fd181011ca486f70272a5e471e5929e2e484e86
anshulsharmaultimate <anshulsharmagwl@gmail.com>
2025-08-16 17:18:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-const BASE_URL = 'http://localhost:3000';
-
-// const BASE_URL = 'https://arv-technical-services.onrender.com'; // Or your actual backend URL
+const BASE_URL = 'https://arv-technical-services.onrender.com'; // Or your actual backend URL
